9c849bdbd450a3b8f8f0fe675db02e4db15a0ac0
hiereit-dev <https://github.com/hiereit-dev>
2024-03-09 16:37:46 +0900

    Initial Commit

---
+++ MemoZip/MemoZip/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MemoZip
+//
+//  Created by 박세라 on 3/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    /* 화면 테스트
+        setBackGroundUI()
+    }
+    
+    func setBackGroundUI() {
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "secret.jpeg")
+        backgroundImage.contentMode = .scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+    }
+     */
+}
+
