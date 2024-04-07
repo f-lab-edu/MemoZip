08835736a602b1ac465a04e4d4008e80c68fac86
frank <frank@f-lab.kr>
2024-04-07 16:38:06 -0400

    SPM. Extract Model module.

--- MyLibrary/Package.swift
+++ MyLibrary/Package.swift
@@ -6,48 +6,56 @@ import PackageDescription
 let package = Package(
     name: "MyLibrary",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Libraries",
-            targets: ["MyLibrary"]),
+            targets: ["Model", "MyLibrary"]
+        ),
         .library(
             name: "Model",
-            targets: ["MyLibrary"]),
+            targets: ["Model"]
+        ),
         .library(
             name: "ViewModel",
-            targets: ["MyLibrary"]),
+            targets: ["MyLibrary"]
+        ),
     ],
-    
+
     dependencies: [
-            // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.0")),
         .package(url: "https://github.com/ReactorKit/ReactorKit.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
         .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", from: "5.0.0"),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/roberthein/TinyConstraints", .upToNextMajor(from: "4.0.1"))
-        
+        .package(url: "https://github.com/roberthein/TinyConstraints", .upToNextMajor(from: "4.0.1")),
+
     ],
-    
+
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
+            name: "Model",
+            path: "Model"
+        ),
+        .target(
             name: "MyLibrary",
-            dependencies: ["RxSwift",
-                           .product(name: "RxCocoa", package: "RxSwift"),
-                           "RxDataSources",
-                           "ReactorKit",
-                           "TinyConstraints",
-                           "Alamofire",
-            ]
+            dependencies: [
+                "Model",
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "ReactorKit", package: "ReactorKit"),
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "RxDataSources", package: "RxDataSources"),
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "TinyConstraints", package: "TinyConstraints"),
+            ],
+            path: "Sources"
         ),
         .testTarget(
             name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
-
+            dependencies: ["MyLibrary"]
+        ),
     ]
 )
