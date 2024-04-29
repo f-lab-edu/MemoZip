2a12f4a3149cc733bb9b15d143c249ad2101946b
hiereit-dev <https://github.com/hiereit-dev>
2024-04-29 19:37:32 +0900

    Feat4: 메모생성화면 이동

--- MemoZip/MemoZip/AppDelegate.swift
+++ MemoZip/MemoZip/AppDelegate.swift
@@ -19,23 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        /*
-        let flowLayout = UICollectionViewFlowLayout()
         
-        let mainViewController = ReminderListViewController(collectionViewLayout: flowLayout) // 맨 처음 보여줄 ViewController
-        
-        // 앱을 시작할 때 루트 뷰 컨트롤러를 생성하고 UINavigationController에 추가합니다.
-        let navigationController = UINavigationController(rootViewController : mainViewController) // 실제로 사용하는 뷰 컨트롤러의 클래스명
-        
-        // NavigationController에 처음으로 보여질 화면을 rootView로 지정
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-         */
         let tabBarController = UITabBarController()
         
         let reminderListViewController = UINavigationController(rootViewController: ReminderListViewController(collectionViewLayout: UICollectionViewFlowLayout()))
         
-        //let reactor = HomeViewReactor()
         let homeViewController = UINavigationController(rootViewController:  HomeViewController(collectionViewLayout: UICollectionViewFlowLayout()))
         
         tabBarController.setViewControllers([homeViewController, reminderListViewController], animated: true)
---
+++ MyLibrary/Sources/Libraries/AddMemoViewController/AddMemoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddMemoViewController.swift
+//
+//
+//  Created by 박세라 on 4/24/24.
+//
+
+import Foundation
+import UIKit
+
+
+public class AddMemoViewController: UIViewController {
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        print("AddMemoViewController viewDidLoad")
+    }
+}
